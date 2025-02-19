c78e1b62b22729ccf3a17d9e3c12bd09a07b7d0d
JeroenIT <jeroenIT@hotmail.com>
2025-02-19 15:21:39 +0100

    Full project tidy

--- Refactor Large Method Kata (Intermediate)/Program.cs
+++ Refactor Large Method Kata (Intermediate)/Program.cs
@@ -4,97 +4,69 @@
     {
         static void Main(string[] args)
         {
-            //ask for input of assignment scores
-            double[] ScoreAssignment = ReadAssignmentScores();
-            //ask for input of quiz scores
-            double[] ScoreQuiz = ReadQuizScores();
-            //ask for input of exam scores
-            double[] ScoreExam = ReadExamScores(); 
-            //calculate final grade
+            // make new double array for scores and check if they are valid
+            double[] ScoreAssignment = GetValidScores("assignment");
+            double[] ScoreQuiz = GetValidScores("exam");
+            double[] ScoreExam = GetValidScores("quiz"); 
+            // make new double for averages
             Double averageAssignment = CalculateAverage(ScoreAssignment);
             Double averageQuiz = CalculateAverage(ScoreQuiz);
             Double averageExam = CalculateAverage(ScoreExam);
-            //display final grade
-            DisplayFinalGrades(averageAssignment);
-            DisplayFinalGrades(averageQuiz);
-            DisplayFinalGrades(averageExam);
+            // display final grade
+            DisplayFinalGrades("assignment",averageAssignment);
+            DisplayFinalGrades("quiz",averageQuiz);
+            DisplayFinalGrades("exam",averageExam);
         }
     
-        //Read assignment scores and convert them to (arrays) double double
-        static double[] ReadAssignmentScores()
+      
+
+        // Ask for scores and and convert them to arrays and validate the input
+        
+        static string[] ReadScores(string type)
         {
-            Console.WriteLine("Enter assignment scores (separated by spaces): ");
-            string[] assignmentScoresInput = Console.ReadLine().Split(' ');
-            double[] assignmentScores = Array.ConvertAll(assignmentScoresInput, double.Parse);
-            return assignmentScores;
+            Console.WriteLine($"Enter {type} scores (separated by spaces): ");
+            string scoresInput = Console.ReadLine() ?? string.Empty;
+            return scoresInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
         }
 
-        //Read quiz scores and convert them to (arrays) double double
-        // Try to add valudation on the input
-        static double[] ReadQuizScores()
+        static bool CheckScores(string[] scoreInputDevided, out double[] scores)
         {
-            //double[] score;
             double score;
+            scores = new double[scoreInputDevided.Length];
+            int counter = 0;
+            bool valid = scoreInputDevided.Length >= 1;
+            foreach (string input in scoreInputDevided)
+            {
+                if (double.TryParse(input, out score))
+                {
+                    scores[counter] = score;
+                    counter++;
+                }
+                else
+                {
+                    valid = false;
+                    Console.WriteLine($"Please enter a valid score. Wrong input: {input}");
+                }
+
+            }
+            return valid;
+        }
+        static double[] GetValidScores(string type)
+        {
             bool valid = false;
-            double[] quizScores = new double[0];
+            double[] scores = new double[0];
 
             while (!valid)
             {
-                Console.WriteLine("Enter quiz scores (separated by spaces): ");
-                string scoresInput = Console.ReadLine() ?? string.Empty;
-                string[] quizScoresInput = scoresInput.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                quizScores = new double[quizScoresInput.Length];
-                int counter = 0;
-                valid = quizScoresInput.Length >= 1;
-                foreach (string input in quizScoresInput)
-                {
-                    if (double.TryParse(input, out score) == true)
-                    {
-                        quizScores[counter] = score;
-                        counter++;
-                        
-                    }
-                    else
-                    {
-                        valid = false;
-                        Console.WriteLine($"Please enter a valid Quiz score {input}.");
-                        //break;
-                        
-                    }
+                string[] scoreInputDevided = ReadScores(type);
 
-                }
+                valid = CheckScores(scoreInputDevided, out scores);
             }
-            return quizScores;
+            
+            return scores;
         }
-        
-
-        //Validate quiz scores????
-/*static bool validateQuizScores(double[] quizScores, out double[] quizScoresfalse)
-        {
-            bool valid = true;
-            quizScoresfalse = null;
-            foreach (var score in quizScores)
-            {
-                if (score < 0 || score > 100)
-                {
-                    valid = false;
-                    quizScoresfalse = quizScores;
-                    break;
-                }
-            }
-            return valid;
-        } */
-        
-
-        //Read exam scores and convert them to (arrays) double double
-        static double[] ReadExamScores()
-        {
-            Console.WriteLine("Enter exam scores (separated by spaces): ");
-            string[] examScoresInput = Console.ReadLine().Split(' ');
-            double[] examScores = Array.ConvertAll(examScoresInput, double.Parse);
-            return examScores;
-        } 
-
+             
+     
         //Calculate average of scores call upon with (scores) in (Arrays) double double
         static double CalculateAverage(double[] scores) 
         {
@@ -106,9 +78,9 @@
                 return sum / scores.Length;
         }
 
-        static void DisplayFinalGrades(Double average)
+        static void DisplayFinalGrades(string type, Double average)
         {
-            Console.WriteLine($"The final grade is: {average}");
+            Console.WriteLine($"The final {type} grade is: {average}");
         }
         
            
